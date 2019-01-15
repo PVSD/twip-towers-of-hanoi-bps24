09095ada4355def6f28f91a7f85fc4f1fbb1e805
bryce_000 <bs482@pvsd.org>
2019-01-14 21:54:58 -0500

    finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,39 +1,22 @@
 package com.company;
 import java.util.*;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         Scanner k = new Scanner(System.in);
         System.out.println("Insert number of disks: ");
 
-        int a = k.nextInt();
-        int i,b,j,l;
-
+        int a = k.nextInt(),j;
         int [] pos = new int [a];
 
-        System.out.println(Arrays.toString(pos));
-        l=(int)Math.pow(2,a);
-
-        for(i=1;i<l;i++)
-        {
-            b=Integer.parseInt(Integer.toBinaryString(i));
+        for(long i=1;i<Math.pow(2,a);i++) {
+            String b = Long.toBinaryString(i);
             for(j=1;j<a;j++)
-            {
-                if(b%10==1) break;
-                else b/=10;
-            }
-            System.out.println("Move disk " + j);
-            if(j%2!=a%2)
-            {
-                pos[j-1]=pos[j-1]+1;
-            }
-            else
-            {
-                pos[j-1]=pos[j-1]-1;
-            }
-            System.out.println(Arrays.toString(pos));
+                if(b.substring(b.length()-j,b.length()-j+1).equals("1"))
+                    break;
+            if(j%2!=a%2) pos[j-1]=pos[j-1]+1;
+            else pos[j-1]=pos[j-1]+2;
+            System.out.println("Move disk " + j + " to peg " + (pos[j-1]%3+1));
         }
     }
 }
