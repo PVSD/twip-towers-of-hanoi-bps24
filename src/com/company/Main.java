04be67dc89ab4e22b142083e51b7955784792b33
bryce_000 <bs482@pvsd.org>
2019-01-14 22:08:34 -0500

    finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,14 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner k = new Scanner(System.in);
-        System.out.println("Insert number of disks: ");
-
-        int a = k.nextInt(),j;
-        int [] pos = new int [a];
-
-        for(long i=1;i<Math.pow(2,a);i++) {
+        System.out.println("Insert number of disks: (1-15 please) ");
+        int inp = k.nextInt(),j;
+        int [] pos = new int [inp];
+        for(long i=1;i<Math.pow(2,inp);i++) {
             String b = Long.toBinaryString(i);
-            for(j=1;j<a;j++)
-                if(b.substring(b.length()-j,b.length()-j+1).equals("1"))
-                    break;
-            if(j%2!=a%2) pos[j-1]=pos[j-1]+1;
+            for(j=1;j<inp;j++)
+                if(b.substring(b.length()-j,b.length()-j+1).equals("1")) break;
+            if(j%2!=inp%2) pos[j-1]=pos[j-1]+1;
             else pos[j-1]=pos[j-1]+2;
             System.out.println("Move disk " + j + " to peg " + (pos[j-1]%3+1));
         }
